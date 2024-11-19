60b84081ef69d3a846e85bbd9b27b7d01fb1bf56
mdauwal <pricelezzchap@gmail.com>
2024-11-19 11:36:17 +0000

    abstract typescript

--- object.ts
+++ object.ts
@@ -1,19 +1,81 @@
-const dog: {firstName: string, lastName: string, age: number} = {
-    firstName: 'rex',
-    lastName: 'Yuppy',
-    age: 20
-}
-
-const printName = (person: {firstName: string, lastName: string}) {
-    console.log(`${person.firstName} ${person.lastName}`);
+abstract class LeadwayEmployee {
     
-}
+    abstract calculatePay(): number;
+  
+    describe(): void {
+      console.log("Empoyee salary");
+    }
+  }
 
-printName({firstName: 'Muhammad', lastName: 'Auwal'})  
+class Frontend extends LeadwayEmployee {
+    constructor(private pay: number) {
+      super();
+    }
+  
+    calculatePay(): number {
+      return this.pay ** 60;
+    }
+  }
+  class Backend extends LeadwayEmployee {
+    constructor(private pay: number) {
+      super();
+    }
+  
+    calculatePay(): number {
+      return this.pay ** 60;
+    }
+  }
+  class Mobile extends LeadwayEmployee {
+    constructor(private pay: number) {
+      super();
+    }
+  
+    calculatePay(): number {
+      return this.pay ** 60;
+    }
+  }
+  
+  const frontend = new Frontend(5);
+  const backend = new Backend(10);
+  const mobile = new Mobile(7);
 
-const houseKeeper1 = (housekeeper: {hname, age, yearsOfExperience, fullname}) {
-    hname = "Mary",
-    age = 30,
-    yearsOfExperience = 5
-    fullAddress = "Kaduna - Nigeria"
-}
+  console.log(frontend.calculatePay()); 
+
+  frontend.describe();
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const dog: {firstName: string, lastName: string, age: number} = {
+//     firstName: 'rex',
+//     lastName: 'Yuppy',
+//     age: 20
+// }
+
+// const printName = (person: {firstName: string, lastName: string}) {
+//     console.log(`${person.firstName} ${person.lastName}`);
+    
+// }
+
+// printName({firstName: 'Muhammad', lastName: 'Auwal'})  
+
+// const houseKeeper1 = (housekeeper: {hname, age, yearsOfExperience, fullname}) {
+//     hname = "Mary",
+//     age = 30,
+//     yearsOfExperience = 5
+//     fullAddress = "Kaduna - Nigeria"
+// }
--- with-typescript.ts
+++ with-typescript.ts
@@ -1,11 +1,36 @@
-function add(a: number, b: number): number {
-    return a + b
+abstract class Animal {
+    abstract makeSound(): void
+    move(): void {
+        console.log("moving..");
+        
+     }
 }
 
-const result = add(5, 3);
-
-console.log(result);
-
-function randomCoordinate (): {x: number, y: number} {
-    return {x: 5, y: 9}
+class Dog extends Animal {
+    makeSound(): void {
+        console.log("Bark!");
+        
+    }
 }
+
+const dog = new Dog()
+
+
+
+
+
+
+
+
+
+// function add(a: number, b: number): number {
+//     return a + b
+// }
+
+// const result = add(5, 3);
+
+// console.log(result);
+
+// function randomCoordinate (): {x: number, y: number} {
+//     return {x: 5, y: 9}
+// }
