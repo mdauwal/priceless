fd477ad389d952e29530700e3a84a0afc57871e3
mdauwal <pricelezzchap@gmail.com>
2024-11-20 08:01:43 +0000

    generics exercises

--- no-typescript.js
+++ no-typescript.js
@@ -7,12 +7,22 @@ const result = add(2, 5);
 console.log(result);
 
 
-const houseKeeper {
-    name = "Mary",
-    age = 30,
-    address = "Abuja, Nigeria",
-    experience = 5,
-    languageSpoken = ["Hausa", "Igbo", "Yoruba", "English"]
+// const houseKeeper = {
+//     name: "Mary",
+//     age: 30,
+//     address: "Abuja, Nigeria",
+//     experience: 5,
+//     languageSpoken: ["Hausa", "Igbo", "Yoruba", "English"]
+// }
+
+function houseKeeper (name, age, hasWorkPermit, languages) {
+    this.name = name;
+    this.age = age;
+    this.hasWorkPermit = hasWorkPermit;
+    this.languages = languages;
 }
 
-console.log("My name is " + houseKeeper.name);
+var houseKeeper1 = new houseKeeper ("Abdul", 20, true, ["Hausa", "Igbo", "Yoruba", "English"])
+
+console.log(houseKeeper);
+
